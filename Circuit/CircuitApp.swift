56419306520dbf47bb42310b163b76679b7b97ad
Nirav <niravpatelp129@gmail.com>
2025-06-02 02:46:12 -0400

    Initial Commit

---
+++ Circuit/CircuitApp.swift
@@ -0,0 +1,17 @@
+//
+//  CircuitApp.swift
+//  Circuit
+//
+//  Created by nirav patel on 2025-06-02.
+//
+
+import SwiftUI
+
+@main
+struct CircuitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CircuitTests/CircuitTests.swift
@@ -0,0 +1,17 @@
+//
+//  CircuitTests.swift
+//  CircuitTests
+//
+//  Created by nirav patel on 2025-06-02.
+//
+
+import Testing
+@testable import Circuit
+
+struct CircuitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
